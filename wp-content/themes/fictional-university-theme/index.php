211466d3140bb08134b12fd5efbaaecc42eba2e6
Doug Franklin <doug@dougfranklin.com>
2020-03-07 13:37:37 -0500

    Arrays

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,15 +1,14 @@
 <?php 
-  function myFirstFunction() {
-      echo "<p>Hello, this is my first function.</p>";
-  }
+  
+  $myName = "Doug";
 
-  function greet($name, $color) {
-      echo "<p>Hi, my name is <b><font color=\"$color\">$name</font></b> and my favorite color is <font color=\"$color\">$color</font>.</p>";
-  }
+  $names = array("Doug", "Aline", "Dubhe", "Gilberto");
 
-  greet('John', 'blue');
-  greet('Jane', 'green');
 ?>
 
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
+<p>Hi, my name is <?php echo $myName; ?></p>
+
+<p>Hi, my name is <?php echo $names[0]; ?></p>
+<p>Hi, my name is <?php echo $names[1]; ?></p>
+<p>Hi, my name is <?php echo $names[2]; ?></p>
+<p>Hi, my name is <?php echo $names[3]; ?></p>
