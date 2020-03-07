e4fab9a694640aaa8e15587860c1de8fcde0e5e5
Doug Franklin <doug@dougfranklin.com>
2020-03-07 13:43:37 -0500

    While Loop

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -6,9 +6,17 @@
 
 ?>
 
-<p>Hi, my name is <?php echo $myName; ?></p>
-
 <p>Hi, my name is <?php echo $names[0]; ?></p>
 <p>Hi, my name is <?php echo $names[1]; ?></p>
 <p>Hi, my name is <?php echo $names[2]; ?></p>
 <p>Hi, my name is <?php echo $names[3]; ?></p>
+
+<?php 
+
+  $count = 1;
+  while($count < 100) {
+    echo "<li>$count</li>";
+    $count++;
+  }
+
+?>
