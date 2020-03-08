1f4086130615cce1f787044bd6d44a5e09bcc791
Doug Franklin <doug@dougfranklin.com>
2020-03-08 09:36:05 -0400

    Final Header and Footer

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,7 +1,4 @@
-<?php get_header(); ?>
-<h1>This is the header area.</h1>
-
-<?php 
+<?php get_header();
 
   while(have_posts()) {
 
@@ -10,10 +7,8 @@
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php the_content(); ?>
     <hr>
-
   <?php }
 
-?>
+  get_footer();
 
-<p>This is the footer area.</p>
-<?php get_footer(); ?>
+?>
