4e54aefa61f3a5f649c137d3f373ad79bcbcd958
Doug Franklin <doug@dougfranklin.com>
2020-03-08 09:55:56 -0400

    Font Awesome and Google Fonts

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -2,6 +2,9 @@
 
   function university_files() {
 
+    wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
      // arg1 (handle): nickname for main css stylesheet wp_enqueue_script for JavaScript
      // arg2 (src):    location to points to the file
       wp_enqueue_style( 'university_main_styles', get_stylesheet_uri());
